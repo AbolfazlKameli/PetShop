6fdcf702a3ced3e9bffd81281544696ad59cb700
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-04 18:48:41 +0330

    feat: Add Users model admin page and forms.

--- petshop/users/admin.py
+++ petshop/users/admin.py
@@ -1,3 +1,47 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User
+from .forms import UserCreationForm, UserChangeForm
+
+
+class UserModelAdmin(BaseUserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+
+    list_display = ('username', 'email', 'phone_number', 'role', 'is_active')
+    list_filter = ('role', 'is_active', 'is_superuser')
+    readonly_fields = ('last_login',)
+
+    fieldsets = (
+        ('Main',
+         {'fields':
+             (
+                 'username',
+                 'email',
+                 'phone_number',
+                 'first_name',
+                 'last_name',
+                 'role',
+                 'password',
+                 'last_login'
+             )
+         }),
+        ('Permissions', {'fields': ('is_active', 'is_superuser', 'groups', 'user_permissions')})
+    )
+    add_fieldsets = (
+        (None, {'fields': ('username', 'email', 'phone_number', 'password1', 'password2')}),
+    )
+
+    search_fields = ('email', 'username', 'phone_number')
+    ordering = ('-created_date',)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        if not is_superuser:
+            form.base_fields['is_superuser'].disabled = True
+        return form
+
+
+admin.site.register(User, UserModelAdmin)
