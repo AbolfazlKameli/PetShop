727fa268deee5eb1fb1972cb71d02eea46e5dac8
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-04 23:23:28 +0330

    feat: Add selectors to get users by email and phone number.

--- petshop/users/selectors.py
+++ petshop/users/selectors.py
@@ -4,3 +4,11 @@ from .models import User
 def get_all_users() -> list[User]:
     users = User.objects.all()
     return users
+
+
+def get_user_by_phone_number(phone_number: str) -> User:
+    return User.objects.filter(phone_number=phone_number).first()
+
+
+def get_user_by_email(email: str) -> User:
+    return User.objects.filter(email=email).first()
