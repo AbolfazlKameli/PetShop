46066f4961e78c9f41037cfe4a716abb1fdc586c
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-04 22:44:35 +0330

    feat: Add UserRegister API.

--- petshop/users/urls.py
+++ petshop/users/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', views.UsersListAPI.as_view(), name='users-list'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.UserRegisterAPI.as_view(), name='user-register'),
 ]
--- petshop/users/views.py
+++ petshop/users/views.py
@@ -1,8 +1,12 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, GenericAPIView
+from rest_framework.response import Response
 
-from petshop.utils.permissions import IsAdminUser
+from petshop.utils.permissions import IsAdminUser, NotAuthenticatedUser
 from .selectors import get_all_users
-from .serializers import UserSerializer
+from .serializers import UserSerializer, UserRegisterSerializer
+from .services import register, generate_otp_code
+from .tasks import send_email
 
 
 class UsersListAPI(ListAPIView):
@@ -11,3 +15,29 @@ class UsersListAPI(ListAPIView):
     permission_classes = (IsAdminUser,)
     filterset_fields = ['role', 'is_active']
     search_fields = ['email', 'username', 'phone_number', 'first_name', 'last_name']
+
+
+class UserRegisterAPI(GenericAPIView):
+    serializer_class = UserRegisterSerializer
+    permission_classes = (NotAuthenticatedUser,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            vd = serializer.validated_data
+            user = register(email=vd['email'], username=vd['username'], password=vd['password'])
+            otp_code = generate_otp_code(email=user.email)
+            content = f'Your verification code: \n{otp_code}'
+            send_email.delay(
+                email=user.email,
+                content=content,
+                subject='PetShop'
+            )
+            return Response(
+                data={'data': {'message': 'We have sent a verification code to your email'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': serializer.errors}},
+            status=status.HTTP_400_BAD_REQUEST
+        )
