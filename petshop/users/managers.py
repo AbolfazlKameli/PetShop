51bc763917c6846578d25639c8bc670a7f650b3a
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-04 22:41:07 +0330

    fix: Make phone_number optional.

--- petshop/users/managers.py
+++ petshop/users/managers.py
@@ -2,21 +2,19 @@ from django.contrib.auth.models import BaseUserManager
 
 
 class UserManager(BaseUserManager):
-    def create_user(self, username, phone_number, email, password):
+    def create_user(self, username, email, password):
         if not username:
             raise ValueError('Users must have username.')
-        if not phone_number:
-            raise ValueError('Users must have phone number.')
         if not email:
             raise ValueError('Users must have email')
 
-        user = self.model(username=username, phone_number=phone_number, email=email)
+        user = self.model(username=username, email=email)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, phone_number, email, password):
-        user = self.create_user(username, phone_number, email, password)
+    def create_superuser(self, username, email, password):
+        user = self.create_user(username, email, password)
         user.is_active = True
         user.is_superuser = True
         user.save(using=self._db)
