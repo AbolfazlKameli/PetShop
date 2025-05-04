f2e670aa78791d8f48382ef653a31b930ee8857d
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-04 22:41:45 +0330

    feat: Add UserRegister serializer.

--- petshop/users/serializers.py
+++ petshop/users/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.password_validation import validate_password
 from rest_framework import serializers
 
 from .models import User
@@ -7,3 +8,24 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         exclude = ('password', 'is_superuser', 'groups', 'user_permissions')
+
+
+class UserRegisterSerializer(serializers.ModelSerializer):
+    confirm_password = serializers.CharField(min_length=8, required=True, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password', 'confirm_password')
+        extra_kwargs = {
+            'password': {'write_only': True, 'min_length': 8}
+        }
+
+    def validate_confirm_password(self, data):
+        password1 = self.initial_data.get('password', False)
+        if password1 and data and password1 != data:
+            raise serializers.ValidationError('Passwords must be match.')
+        try:
+            validate_password(data)
+        except serializers.ValidationError:
+            raise serializers.ValidationError()
+        return data
