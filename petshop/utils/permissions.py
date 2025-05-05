fd70facf883e3f0f5fa55ad478860c8234e17a2b
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-05 19:25:05 +0330

    feat: Add IsOwner permission.

--- petshop/utils/permissions.py
+++ petshop/utils/permissions.py
@@ -1,5 +1,7 @@
 from rest_framework.permissions import BasePermission
 
+from petshop.users.models import User
+
 
 class IsAdminUser(BasePermission):
     message = 'You dont have permission to perform this action.'
@@ -13,3 +15,17 @@ class NotAuthenticatedUser(BasePermission):
 
     def has_permission(self, request, view):
         return not bool(request.user.is_authenticated)
+
+
+class IsOwnerUser(BasePermission):
+    message = 'You are not the owner.'
+
+    def has_permission(self, request, view):
+        return bool(request.use and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        if isinstance(obj, User):
+            return obj.id == request.user.id
+        if hasattr(obj, 'owner') and obj.owner:
+            return obj.owner.id == request.user.id
+        return False
