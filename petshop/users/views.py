de7a13f4b64a6c03e2e10e89c854fd6d9d2b9de4
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-06 19:45:45 +0330

    fix: Fix document.

--- petshop/users/views.py
+++ petshop/users/views.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 
 from petshop.utils.doc_serializers import TokenResponseSerializer, ResponseSerializer
 from petshop.utils.permissions import IsAdminUser, NotAuthenticatedUser, IsOwnerUser
+from .models import Address
 from .selectors import (
     get_all_users,
     get_user_by_phone_number,
@@ -381,18 +382,27 @@ class DeleteUserAccountAPI(GenericAPIView):
 
 
 class UserAddressesListAPI(ListAPIView):
+    """
+    API for listing authenticated users addresses. Accessible only to the uesr themselves.
+    """
     serializer_class = AddressSerializer
     permission_classes = (IsOwnerUser,)
     search_fields = ('address', 'postal_code')
 
     def get_queryset(self):
+        if getattr(self, "swagger_fake_view", False):
+            return Address.objects.none()
         return get_user_addresses(owner=self.request.user)
 
 
 class AddressCreateAPI(GenericAPIView):
+    """
+    API for creating addresses for the authenticated user. Accessible only to the user themselves.
+    """
     serializer_class = AddressSerializer
     permission_classes = (IsAuthenticated,)
 
+    @extend_schema(responses={201: ResponseSerializer})
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -408,11 +418,15 @@ class AddressCreateAPI(GenericAPIView):
 
 
 class AddressUpdateAPI(GenericAPIView):
+    """
+    API for creating addresses for the authenticated user. Accessible only to the user themselves.
+    """
     serializer_class = AddressSerializer
     permission_classes = (IsOwnerUser,)
     lookup_url_kwarg = 'address_id'
     queryset = get_all_addresses()
 
+    @extend_schema(responses={200: ResponseSerializer})
     def put(self, request, *args, **kwargs):
         address = self.get_object()
         serializer = self.serializer_class(data=request.data, instance=address)
@@ -429,6 +443,9 @@ class AddressUpdateAPI(GenericAPIView):
 
 
 class AddressDeleteAPI(GenericAPIView):
+    """
+    API for deleting addresses for the authenticated user. Accessible only to the user themselves.
+    """
     serializer_class = AddressSerializer
     permission_classes = (IsOwnerUser,)
     lookup_url_kwarg = 'address_id'
