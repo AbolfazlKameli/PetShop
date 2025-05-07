ebf8f4b1f8033a9d97f9f1b55c8f2540b42dbe89
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-07 15:13:46 +0330

    fix: Fix document.

--- petshop/products/views.py
+++ petshop/products/views.py
@@ -1,14 +1,19 @@
+from drf_spectacular.utils import extend_schema
 from rest_framework import status
 from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
+from petshop.utils.doc_serializers import ResponseSerializer
 from petshop.utils.permissions import IsAdminUser
 from .selectors import get_all_categories
 from .serializers import ProductCategorySerializer
 
 
 class ProductCategoriesListAPI(ListAPIView):
+    """
+    API for listing Categories. Accessible to all users.
+    """
     serializer_class = ProductCategorySerializer
     queryset = get_all_categories()
     permission_classes = (AllowAny,)
@@ -16,9 +21,13 @@ class ProductCategoriesListAPI(ListAPIView):
 
 
 class ProductCategoryCreateAPI(GenericAPIView):
+    """
+    API for creating Categories. Accessible only to the admins.
+    """
     serializer_class = ProductCategorySerializer
     permission_classes = (IsAdminUser,)
 
+    @extend_schema(responses={201: ResponseSerializer})
     def post(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
@@ -34,12 +43,16 @@ class ProductCategoryCreateAPI(GenericAPIView):
 
 
 class ProductCategoryUpdateAPI(GenericAPIView):
+    """
+    API for updating Categories. Accessible only to the admins.
+    """
     serializer_class = ProductCategorySerializer
     queryset = get_all_categories()
     permission_classes = (IsAdminUser,)
     lookup_field = 'slug'
     lookup_url_kwarg = 'category_slug'
 
+    @extend_schema(responses={200: ResponseSerializer})
     def put(self, request, *args, **kwargs):
         category = self.get_object()
         serializer = self.serializer_class(data=request.data, instance=category)
@@ -56,6 +69,9 @@ class ProductCategoryUpdateAPI(GenericAPIView):
 
 
 class ProductCategoryDeleteAPI(GenericAPIView):
+    """
+    API for deleting Categories. Accessible only to the admins.
+    """
     serializer_class = ProductCategorySerializer
     queryset = get_all_categories()
     permission_classes = (IsAdminUser,)
