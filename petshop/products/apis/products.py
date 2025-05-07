a619e3c6c090e976f13e2d7e86cfd05ef37d22d8
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-07 16:34:54 +0330

    feat: Add ProductList serializer for list apis.

--- petshop/products/apis/products.py
+++ petshop/products/apis/products.py
@@ -3,11 +3,11 @@ from rest_framework.permissions import AllowAny
 
 from ..filters import ProductFilter
 from ..selectors import get_all_products
-from ..serializers import ProductSerializer
+from ..serializers import ProductListSerializer
 
 
 class ProductsListAPI(ListAPIView):
-    serializer_class = ProductSerializer
+    serializer_class = ProductListSerializer
     permission_classes = (AllowAny,)
     queryset = get_all_products()
     filterset_class = ProductFilter
--- petshop/products/serializers.py
+++ petshop/products/serializers.py
@@ -9,9 +9,15 @@ class ProductCategorySerializer(serializers.ModelSerializer):
         fields = ('title',)
 
 
+class ProductListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ('title', 'unit_price', 'final_price', 'discount_percent')
+
+
 class ProductSerializer(serializers.ModelSerializer):
     category = ProductCategorySerializer(read_only=True)
 
     class Meta:
         model = Product
-        fields = '__all__'
+        exclude = ('slug',)
