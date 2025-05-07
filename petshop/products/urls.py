9a33f467bc6296271a86d8e8955d4532738d43f8
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-07 14:53:31 +0330

    feat: Add CategoryCreate API.

--- petshop/products/urls.py
+++ petshop/products/urls.py
@@ -1,8 +1,14 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
 app_name = 'products'
-urlpatterns = [
+
+categories = [
     path('', views.ProductCategoriesListAPI.as_view(), name='categories-list'),
+    path('create/', views.ProductCategoryCreateAPI.as_view(), name='category-create'),
+]
+
+urlpatterns = [
+    path('categories/', include(categories)),
 ]
--- petshop/products/views.py
+++ petshop/products/views.py
@@ -1,6 +1,9 @@
-from rest_framework.generics import ListAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+from petshop.utils.permissions import IsAdminUser
 from .selectors import get_all_categories
 from .serializers import ProductCategorySerializer
 
@@ -10,3 +13,21 @@ class ProductCategoriesListAPI(ListAPIView):
     queryset = get_all_categories()
     permission_classes = (AllowAny,)
     search_fields = ('title',)
+
+
+class ProductCategoryCreateAPI(GenericAPIView):
+    serializer_class = ProductCategorySerializer
+    permission_classes = (IsAdminUser,)
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'data': {'message': 'Category created successfully.'}},
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            data={'data': {'errors': serializer.errors}},
+            status=status.HTTP_400_BAD_REQUEST
+        )
