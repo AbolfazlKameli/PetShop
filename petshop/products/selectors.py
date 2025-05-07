e7872d719bfa8f297618d94765a697709bc09092
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-07 16:28:30 +0330

    feat: Add a selector that returns all product objects.

--- petshop/products/selectors.py
+++ petshop/products/selectors.py
@@ -1,5 +1,9 @@
-from .models import ProductCategory
+from .models import ProductCategory, Product
 
 
 def get_all_categories() -> list[ProductCategory]:
     return ProductCategory.objects.all()
+
+
+def get_all_products() -> list[Product]:
+    return Product.objects.select_related('category').all()
