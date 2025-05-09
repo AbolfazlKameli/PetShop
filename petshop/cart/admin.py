849e5e62b42d40e4787861afdac1276de32ece17
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-09 22:52:36 +0330

    feat: Add cart item model.

--- petshop/cart/admin.py
+++ petshop/cart/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
 
-from .models import Cart
+from .models import Cart, CartItem
+
+
+class CartItemInline(admin.StackedInline):
+    model = CartItem
 
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
     list_display = ('owner__username', 'id')
     search_fields = ('owner__username',)
+
+    inlines = (CartItemInline,)
+
+
+@admin.register(CartItem)
+class CartItemModelAdmin(admin.ModelAdmin):
+    list_display = ('cart__id', 'product__title', 'quantity', 'id')
+    search_fields = ('product_title',)
--- petshop/cart/models.py
+++ petshop/cart/models.py
@@ -1,6 +1,8 @@
 from django.contrib.auth import get_user_model
+from django.core.validators import MaxValueValidator
 from django.db import models
 
+from petshop.products.models import Product
 from petshop.utils.utils import BaseModel
 
 User = get_user_model()
@@ -8,3 +10,10 @@ User = get_user_model()
 
 class Cart(BaseModel):
     owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
+
+
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cart_items')
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    quantity = models.PositiveSmallIntegerField(default=1, validators=[MaxValueValidator(100)])
+    created_date = models.DateTimeField(auto_now_add=True)
