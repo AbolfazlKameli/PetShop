714843f647be65154fb4002b200a5a20f7ea551e
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-09 22:40:08 +0330

    feat: Add cart model.

--- petshop/cart/admin.py
+++ petshop/cart/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Cart
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ('owner__username', 'id')
+    search_fields = ('owner__username',)
--- petshop/cart/models.py
+++ petshop/cart/models.py
@@ -1,3 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+from petshop.utils.utils import BaseModel
+
+User = get_user_model()
+
+
+class Cart(BaseModel):
+    owner = models.OneToOneField(User, on_delete=models.CASCADE, related_name='cart')
