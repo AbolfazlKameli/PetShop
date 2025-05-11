4f30d235bf14c5d639eeadd6bff095dc98ad620f
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-11 22:39:56 +0330

    feat: Add order items serializer.

--- petshop/orders/serializers.py
+++ petshop/orders/serializers.py
@@ -1,9 +1,25 @@
 from rest_framework import serializers
 
-from .models import Order
+from .models import Order, OrderItem
+
+
+class OrderItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    items = OrderItemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(read_only=True)
+    total_quantity = serializers.SerializerMethodField(read_only=True)
+
+    def get_total_price(self, obj):
+        return obj.get_total_price()
+
+    def get_total_quantity(self, obj):
+        return obj.get_total_quantity()
+
     class Meta:
         model = Order
         fields = '__all__'
