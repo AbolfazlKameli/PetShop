925c5bcbc66a13951ed6b9a73f5ceeb0f5963d26
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-11 22:15:54 +0330

    feat: Add IsOwnerOrAdmin permission.

--- petshop/utils/permissions.py
+++ petshop/utils/permissions.py
@@ -29,3 +29,18 @@ class IsOwnerUser(BasePermission):
         if hasattr(obj, 'owner') and obj.owner:
             return obj.owner.id == request.user.id
         return False
+
+
+class IsOwnerOrAdminUser(BasePermission):
+    message = 'You are not the owner.'
+
+    def has_permission(self, request, view):
+        return bool(request.user and request.user.is_authenticated)
+
+    def has_object_permission(self, request, view, obj):
+        condition: bool = False
+        if isinstance(obj, User):
+            condition = obj.id == request.user.id
+        if hasattr(obj, 'owner') and obj.owner:
+            condition = obj.owner.id == request.user.id
+        return condition or request.user.is_admin
