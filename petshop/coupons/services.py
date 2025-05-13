659043227e167be3b89220559cfad68c4dbca35f
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-13 23:29:50 +0330

    feat: Add apply coupon service.

--- petshop/coupons/services.py
+++ petshop/coupons/services.py
@@ -5,6 +5,15 @@ from .models import Coupon
 
 
 @transaction.atomic
+def apply_coupon(coupon: Coupon, order: Order) -> Order:
+    order.discount_percent += coupon.discount_percent
+    order.coupon = coupon
+    order.full_clean()
+    order.save()
+    return order
+
+
+@transaction.atomic
 def discard_coupon(coupon: Coupon, orders: list[Order]):
     for order in orders:
         order.discount_percent -= coupon.discount_percent
