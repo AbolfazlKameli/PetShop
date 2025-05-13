71ddac6af03f7644ca65550f9f2136cdfc499302
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-13 23:31:34 +0330

    feat: Add CouponOperations Serializer.

--- petshop/coupons/serializers.py
+++ petshop/coupons/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 
+from petshop.orders.selectors import get_pending_orders
 from .models import Coupon
+from .selectors import get_valid_coupons
 
 
 class CouponSerializer(serializers.ModelSerializer):
@@ -12,3 +14,18 @@ class CouponSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coupon
         fields = '__all__'
+
+
+class CouponApplySerializer(serializers.Serializer):
+    coupon = serializers.SlugRelatedField(
+        slug_field='code',
+        queryset=get_valid_coupons(),
+        write_only=True,
+        required=True
+    )
+    order = serializers.PrimaryKeyRelatedField(queryset=get_pending_orders(), write_only=True, required=True)
+
+    def validate_order(self, data):
+        if data.coupon is not None:
+            raise serializers.ValidationError('You can`t apply more than one coupon on an order.')
+        return data
