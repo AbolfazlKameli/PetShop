99acea54651fddcf660d9c5df4cdcf3fe3f64718
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-13 22:03:51 +0330

    fix: Remove discount percent defaul value.

--- petshop/coupons/models.py
+++ petshop/coupons/models.py
@@ -13,7 +13,6 @@ class Coupon(BaseModel):
     code = models.CharField(max_length=50, unique=True, db_index=True)
     discount_percent = models.PositiveSmallIntegerField(
         validators=[MaxValueValidator(100)],
-        default=0,
         db_index=True
     )
     expiration_date = models.DateTimeField(db_index=True)
