0041281cb930252d5c095c42b45c7862dfe24b03
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-14 22:47:11 +0330

    feat: Add a service that chagnese a review status.

--- petshop/products/services.py
+++ petshop/products/services.py
@@ -1,6 +1,6 @@
 from django.db import transaction
 
-from .models import Product, ProductCategory, ProductDetail
+from .models import Product, ProductCategory, ProductDetail, ProductReview
 
 
 @transaction.atomic
@@ -11,3 +11,9 @@ def create_product(data, category: ProductCategory) -> Product:
 def create_product_details(product: Product, data: dict):
     items = [ProductDetail(**item_data, product=product) for item_data in data]
     ProductDetail.objects.bulk_create(items)
+
+
+def change_review_status(review: ProductReview, status: str) -> ProductReview:
+    review.status = status
+    review.save()
+    return review
