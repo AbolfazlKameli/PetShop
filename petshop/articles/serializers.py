840bc1a4b7ffd4f3df5eb397ae6bcee352f7f160
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-15 13:49:42 +0330

    feat: Add ArticleCategory serializer.

---
+++ petshop/articles/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+
+from .models import ArticleCategory
+
+
+class ArticleCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ArticleCategory
+        fields = ('id', 'title')
