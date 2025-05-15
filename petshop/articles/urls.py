9766ae7ea2b1791f88c4e80d860431325840db5b
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-15 17:45:46 +0330

    feat: Add ArticleList API.

--- petshop/articles/urls.py
+++ petshop/articles/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = 'articles'
 
 urlpatterns = [
-
+    path('', views.ArticlesListAPI.as_view(), name='article'),
 ]
--- petshop/articles/views.py
+++ petshop/articles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.generics import ListAPIView
+from rest_framework.permissions import AllowAny
 
-# Create your views here.
+from .selectors import get_all_articles
+from .serializers import ArticlesListSerializer
+
+
+class ArticlesListAPI(ListAPIView):
+    """
+    API for listing Articles. Accessible to all users.
+    """
+    serializer_class = ArticlesListSerializer
+    permission_classes = (AllowAny,)
+    queryset = get_all_articles()
+    search_fields = ('title', 'text')
