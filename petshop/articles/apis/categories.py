ca770a1a094bc509aeba9a94c11758412c40cbcf
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-15 14:00:32 +0330

    feat: Add ArticleCategoryCreate API.

--- petshop/articles/apis/categories.py
+++ petshop/articles/apis/categories.py
@@ -1,7 +1,12 @@
 from drf_spectacular.utils import extend_schema
-from rest_framework.generics import ListAPIView
+from rest_framework import status
+from rest_framework.generics import ListAPIView, GenericAPIView
 from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+from petshop.utils.doc_serializers import ResponseSerializer
+from petshop.utils.exceptions import CustomBadRequest
+from petshop.utils.permissions import IsAdminUser
 from ..selectors import get_all_categories
 from ..serializers import ArticleCategorySerializer
 
@@ -15,3 +20,23 @@ class ArticleCategoriesListAPI(ListAPIView):
     queryset = get_all_categories()
     permission_classes = (AllowAny,)
     search_fields = ('title',)
+
+
+@extend_schema(tags=['Article Categories'])
+class ArticleCategoryCreateAPI(GenericAPIView):
+    """
+    API for creating Categories. Accessible only to admins.
+    """
+    serializer_class = ArticleCategorySerializer
+    permission_classes = (IsAdminUser,)
+
+    @extend_schema(responses={201: ResponseSerializer})
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                data={'data': {'message': 'Category created successfully.'}},
+                status=status.HTTP_201_CREATED
+            )
+        raise CustomBadRequest(serializer.errors)
--- petshop/articles/urls.py
+++ petshop/articles/urls.py
@@ -6,6 +6,7 @@ app_name = 'articles'
 
 categories_paths = [
     path('', categories.ArticleCategoriesListAPI.as_view(), name='categories-list'),
+    path('create/', categories.ArticleCategoryCreateAPI.as_view(), name='category-create'),
 ]
 
 urlpatterns = [
