15bcb903bc89e9c3b2024e8bf4b45c720d917060
Abolfazl Kameli <abolfazlkameli0@gmail.com>
2025-05-15 17:44:55 +0330

    feat: Add ArticleListSerializer.

---
+++ petshop/articles/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+
+from .models import Article
+
+
+class ArticlesListSerializer(serializers.ModelSerializer):
+    text = serializers.SerializerMethodField(read_only=True)
+
+    def get_text(self, obj):
+        return f'{obj.text[:150]}...'
+
+    class Meta:
+        model = Article
+        fields = ('id', 'title', 'slug', 'text', 'image', 'created_date', 'updated_date')
